e667c2a727f86f89ecf4bad3286a7137d9bdb768
erik-r-u-kodehode <125983659+erik-r-u-kodehode@users.noreply.github.com>
2023-06-09 13:15:39 +0200

    fix service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,11 @@
-self.addEventListener('install', function(event) {
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
     event.waitUntil(
-      caches.open('my-cache').then(function(cache) {
-        return cache.addAll([
+        addResourcesToCache([
           'IMAGES/aktiv.png',
           'IMAGES/aktiv.webp',
           'IMAGES/ekstrem.png',
@@ -23,16 +27,14 @@ self.addEventListener('install', function(event) {
           'JSON/profiles.JSON',
           'FONTS/Ultramagnetic_Bold.tff',
           // Add other files and assets you want to cache
-        ]);
-      })
+        ])
     );
   });
   
-  self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
-  });
-  
+});
